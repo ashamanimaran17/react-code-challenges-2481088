2e30c393b1ead16a685685ae05736ff56d31ba07
ashamanimaran17 <ashamanimaran17@gmail.com>
2022-09-28 14:08:57 -0700

    addeventlisterner on doubleclick

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import WindowEvent from './WindowEvent'
 
 export default function ToggleWindowEvent () {
   const [windowEvent, setWindowEvent] = useState(false)
+  function dblClicked(){
+    alert("mouse clicked");
+  }
+  useEffect(()=> {
+if(windowEvent){
+  window.addEventListener("dblclick", dblClicked);
+}else{
+  window.removeEventListener("dblclick", dblClicked);
+}
+  },[windowEvent])
   return (
     <div>
       <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
