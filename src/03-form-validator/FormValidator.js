624a40a2e5db692a3922a7d9b38c5011953d812c
ashamanimaran17 <ashamanimaran17@gmail.com>
2022-09-28 11:51:10 -0700

    form validation completed

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -3,7 +3,28 @@ import { useState } from 'react'
 export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const allErrorMessages = [];
+  const validateForm = (event) => {
+    event.preventDefault();
+    const emailRegexp = new RegExp(/@/);
+    if(!email.match(emailRegexp)){
+      allErrorMessages.push("Invalid email");
+    }
+    const passwordRegExp = new RegExp(/.{8,}/)
+    if(!password.match(passwordRegExp)){
+      allErrorMessages.push("Password length should be more than 8 chars");
+    }
+    if(password !== passwordConfirm){
+      allErrorMessages.push("Password confirmation does not match");
+    }
+    if(!allErrorMessages.length){
+      allErrorMessages.push("Success");
+    }
+    setErrorMessage(allErrorMessages.join(","))
+
+  }
 
   return (
     <form>
@@ -23,7 +44,8 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      {errorMessage}
+      <input type='submit' value='Submit' onClick={(event) => validateForm(event)}/>
     </form>
   )
 }
