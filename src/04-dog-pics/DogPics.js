76fcb86e01bff714c048871d54abc95974731d69
ashamanimaran17 <ashamanimaran17@gmail.com>
2022-09-28 12:14:10 -0700

    dog pics ok

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,22 @@
+import {useState, useEffect, useCallback} from "react";
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+  const [currentDog, setCurrentDog] = useState();
+  const url = "https://dog.ceo/api/breeds/image/random";
+  const fetchNewDog = useCallback(() => {
+  fetch(url).then((resp)=> {
+    return (resp.json())
+  }).then((json) => {
+    setCurrentDog(json.message);
+  })
+  }, [url, setCurrentDog])
+  useEffect(() => {
+    fetchNewDog();
+  }, [fetchNewDog])
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={currentDog} alt="dog" height={"300px"}/>
+      <button onClick={fetchNewDog}>🐶</button>
     </div>
   )
 }
